0279221ab332e9f5e81d4dd14bae7e4e9267fb5a
Dennis Slobodzian <de.slobodzian@gmail.com>
2021-05-17 17:49:00 -0400

    cmake fixes, added zed camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
-int main() {
-	return 0;
+#include <sl/Camera.hpp>
+
+using namespace std;
+using namespace sl;
+
+
+int main(int argc, char **argv) {
+	Camera zed;
+
+	ERROR_CODE returned_state = zed.open();
+
+	if (returned_state != ERROR_CODE::SUCCESS) {
+		cout << "Error " << returned_state << ", exit program.\n";
+		return EXIT_FAILURE;
+	}
+
+	auto camera_infos = zed.getCameraInformation();
+	printf("Hello! This is my serial number: %d\n", camera_infos.serial_number);
+	zed.close();
+	return EXIT_SUCCESS; 
 }

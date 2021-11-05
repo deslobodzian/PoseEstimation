df6763d53e77f397528079761f724c5fc3310370
DSlobodzian <DSlobodzian@DESKTOP-746BI30>
2021-11-05 13:08:47 -0400

    adding zed class

---
+++ cpp/cmake-build-debug/CMakeFiles/pose_estimation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_estimation.dir/src/main.cpp.obj"
+  "pose_estimation.exe"
+  "pose_estimation.exe.manifest"
+  "pose_estimation.lib"
+  "pose_estimation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

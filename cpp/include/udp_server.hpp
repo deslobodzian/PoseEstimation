c797b7bc558f58bed3998cc517a9c16de589d764
deslobodzian <deslobodzian@github.com>
2022-02-04 11:31:42 -0500

    working UDP?

--- cpp/include/udp_server.hpp
+++ cpp/include/udp_server.hpp
@@ -30,10 +30,8 @@ private:
 
 public:
     Server(const std::string& host, int host_port, const std::string& client, int client_port) {
-        char h[BUFFER_SIZE];
-        host.copy(h, host.length()+1);
-        char c[BUFFER_SIZE];
-        client.copy(c, client.length()+1);
+        const char *h = host.c_str();
+        const char *c = client.c_str();
         host_port_ = host_port;
         client_port_ = client_port;
 
@@ -51,7 +49,7 @@ public:
 
         bzero((char* ) &serverAddr_, sizeof(serverAddr_));
         serverAddr_.sin_family = AF_INET;
-        serverAddr_.sin_addr.s_addr = inet_addr("10.56.87.59");
+        serverAddr_.sin_addr.s_addr = inet_addr(h);
         serverAddr_.sin_port = htons((unsigned short)host_port_);
 
         if (bind(socket_, ((struct sockaddr *) &serverAddr_), sizeof(serverAddr_)) < 0) {
@@ -60,7 +58,7 @@ public:
 
         bzero((char* ) &clientAddr_, sizeof(clientAddr_));
         clientAddr_.sin_family = AF_INET;
-        clientAddr_.sin_addr.s_addr = inet_addr("10.56.87.2");
+        clientAddr_.sin_addr.s_addr = inet_addr(c);
         clientAddr_.sin_port = htons((unsigned short)client_port_);
         clientLength_ = sizeof(clientAddr_);
     }

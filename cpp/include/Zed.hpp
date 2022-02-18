f9131db72c35fa2aadff562dc38a5dfabff9c1b7
deslobodzian <de.slobodzian@gmail.com>
2022-02-18 11:23:46 -0500

    angle to catapult

--- cpp/include/Zed.hpp
+++ cpp/include/Zed.hpp
@@ -93,9 +93,9 @@ public:
         tmp.tx = CAM_TO_ROBOT_X;
         tmp.ty = CAM_TO_ROBOT_Y + ty;
 //        transform_pose(tmp, 0, ty, 0);
-        info("tx" + std::to_string(tmp.tx));
-        info("ty" + std::to_string(tmp.ty));
-        info("tz" + std::to_string(tmp.tz));
+//        info("tx" + std::to_string(tmp.tx));
+//        info("ty" + std::to_string(tmp.ty));
+//        info("tz" + std::to_string(tmp.tz));
         float x = pow(object.position.x - tmp.tx, 2);
         float y = pow(object.position.y - tmp.ty, 2);
         float z = pow(object.position.z - tmp.tz, 2);

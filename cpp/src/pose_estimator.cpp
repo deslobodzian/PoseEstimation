705093bc5d4eb3662ba2951127ac0f3c3c098961
deslobodzian <de.slobodzian@gmail.com>
2022-03-19 12:08:57 -0400

    why

--- cpp/src/pose_estimator.cpp
+++ cpp/src/pose_estimator.cpp
@@ -172,12 +172,12 @@ void PoseEstimator::send_message() {
     if (num_monocular_cameras_ > 0) {
 	info("Number of cameras: " + std::to_string(num_monocular_cameras_));
 //        temp = monocular_cameras_.at(0).get_objects(0);
-        tracked_object b_ball = monocular_cameras_.at(0).closest_object_to_camera(1);
+        tracked_object b_ball = monocular_cameras_.at(0).closest_object_to_camera(blue_ball);
 //        tracked_object r_ball = monocular_cameras_.at(0).closest_object_to_camera(red_ball);
         // id of 99 is a dummy id, no current object has this object id.
-//        if (b_ball.class_id != 99) {
-//            blue_ball_yaw = monocular_cameras_.at(0).yaw_angle_to_object(b_ball);
-//        }
+        if (b_ball.class_id != 99) {
+            blue_ball_yaw = monocular_cameras_.at(0).yaw_angle_to_object(b_ball);
+        }
 //        if (r_ball.class_id != 99) {
 //            red_ball_yaw = monocular_cameras_.at(0).yaw_angle_to_object(r_ball);
 //        }

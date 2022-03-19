1c6511b54bf8e5fd362b251dbccd125462c2b252
deslobodzian <de.slobodzian@gmail.com>
2022-03-19 12:31:28 -0400

    might work

--- cpp/src/pose_estimator.cpp
+++ cpp/src/pose_estimator.cpp
@@ -173,14 +173,14 @@ void PoseEstimator::send_message() {
 	info("Number of cameras: " + std::to_string(num_monocular_cameras_));
 //        temp = monocular_cameras_.at(0).get_objects(0);
         tracked_object b_ball = monocular_cameras_.at(0).closest_object_to_camera(0);
-//        tracked_object r_ball = monocular_cameras_.at(0).closest_object_to_camera(red_ball);
+        tracked_object r_ball = monocular_cameras_.at(0).closest_object_to_camera(2);
         // id of 99 is a dummy id, no current object has this object id.
         if (b_ball.class_id != 99) {
             blue_ball_yaw = monocular_cameras_.at(0).yaw_angle_to_object(b_ball);
         }
-//        if (r_ball.class_id != 99) {
-//            red_ball_yaw = monocular_cameras_.at(0).yaw_angle_to_object(r_ball);
-//        }
+        if (r_ball.class_id != 99) {
+            red_ball_yaw = monocular_cameras_.at(0).yaw_angle_to_object(r_ball);
+        }
     }
     info("Closest red ball yaw" + std::to_string(red_ball_yaw));
     info("Closest blue ball yaw" + std::to_string(blue_ball_yaw));

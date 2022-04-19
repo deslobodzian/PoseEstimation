663a40d348adb0db2942f4c238f46f6bc354e336
deslobodzian <deslobodzian@github.com>
2022-04-19 19:36:24 -0400

    zmq stuff

---
+++ cpp/include/networking/zmq_server.hpp
@@ -0,0 +1,36 @@
+//
+// Created by DSlobodzian on 4/19/2022.
+//
+
+#ifndef POSE_ESTIMATION_ZMQ_SERVER_HPP
+#define POSE_ESTIMATION_ZMQ_SERVER_HPP
+
+#include "zmq/zmq.hpp"
+#include <iostream>
+#include "utils.hpp"
+
+class ZMQServer {
+private:
+    zmq::socket_t _socket;
+    std::string _address = "tcp://*:27001";
+public:
+    ZMQServer() {
+        zmq::context_t ctx;
+        _socket = zmq::socket_t(ctx, zmq::socket_type::pair);
+        // bind socket
+        info("Binding Socket: " + _address);
+        _socket.bind(_address);
+    }
+    ~ZMQServer() = default;
+
+    void send_message(std::string message) {
+        _socket.send(zmq::buffer(message), zmq::send_flags::dontwait);
+    }
+
+    void receive_message() {
+        zmq::message_t incoming_frame;
+        _socket.recv(incoming_frame, zmq::recv_flags::none);
+        info("Incoming frame {" + incoming_frame.str() + "}");
+    }
+};
+#endif //POSE_ESTIMATION_ZMQ_SERVER_HPP
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -6,24 +6,25 @@
 #include "utils.hpp"
 #include "Zed.hpp"
 #include "pose_estimator.hpp"
-#include "networking/udp_server.hpp"
-#include "particle_filter.hpp"
+#include "networking/zmq_server.hpp"
 
 
 int main() {
     // if c++20 can replace with " Landmark{.x = 3, .y = -2, .id = 3} "
-    Map map;
+//    Map map;
     // estimator(monocular cam, zed cam, landmarks)
-    PoseEstimator estimator(0, 1, map.get_landmarks());
-    std::vector<Eigen::Vector3d> z;
-    estimator.init();
+//    PoseEstimator estimator(0, 1, map.get_landmarks());
+//    std::vector<Eigen::Vector3d> z;
+//    estimator.init();
+    ZMQServer server;
 
     while (true) {
+        server.receive_message();
         // wait until the estimator has started all threads before feeding data to the filter.
-        if (estimator.threads_started()) {
+//        if (estimator.threads_started()) {
 	    //auto start = std::chrono::high_resolution_clock::now();
             //estimator.print_zed_measurements(0);
-            estimator.send_message();
+//            estimator.send_message();
 	    //auto stop = std::chrono::high_resolution_clock::now();
 	    //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop-start);
 	    //info("Duration: " + std::to_string(duration.count()));

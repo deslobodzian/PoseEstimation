06e45b143ce9fa7a8ad82104e6a616106e6995d4
deslobodzian <deslobodzian@github.com>
2022-04-19 21:00:01 -0400

    zmq testing

--- cpp/include/networking/zmq_server.hpp
+++ cpp/include/networking/zmq_server.hpp
@@ -13,10 +13,10 @@ class ZMQServer {
 private:
     zmq::socket_t _socket;
     std::string _address = "tcp://*:5555";
+    zmq::context_t _ctx;
 public:
     ZMQServer() {
-        zmq::context_t ctx;
-        _socket = zmq::socket_t(ctx, zmq::socket_type::dealer);
+        _socket = zmq::socket_t(_ctx, zmq::socket_type::dealer);
         // bind socket
         info("Binding Socket: " + _address);
         _socket.bind(_address);

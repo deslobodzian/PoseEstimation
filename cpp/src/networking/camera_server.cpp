198086e2aa7d800caf6144159a36271ec77e3398
Dennis Slobodzian <de.slobodzian@gmail.com>
2022-04-26 15:37:21 -0400

    camera servers?

--- cpp/src/networking/camera_server.cpp
+++ cpp/src/networking/camera_server.cpp
@@ -4,12 +4,12 @@
 #include "networking/camera_server.hpp"
 
 CameraServer::CameraServer(MonocularCamera camera) {
-    frc::CameraServer::StartAutomaticCapture();
-//    cv_source_ = frc::CameraServer::PutVideo("Inference Image", 640, 480);
-//    camera_ = camera;
+    cv_source_ = frc::CameraServer::PutVideo("Inference Image", 640, 480);
+    frc::CameraServer::AddServer("Why");
+    camera_ = camera;
 }
 
 void CameraServer::sendFrame(cv::Mat image) {
-//    cv_source_.PutFrame(image);
+    cv_source_.PutFrame(image);
 }
 
